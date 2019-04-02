472f0eae77a0e19eaa55b847cd675b8d3a670503
minimumwriting <47500894+minimumwriting@users.noreply.github.com>
2019-04-02 22:31:37 +0900

    Update Two Integers.cpp

--- Two Integers.cpp
+++ Two Integers.cpp
@@ -1,7 +1,11 @@
 /*
+
+https://atcoder.jp/contests/apc001/tasks/apc001_a
+
 x의 배수 중 y의 배수가 아닌 것을 찾는 문제
 x역시 x의 배수이다.
 그렇다면 x가 y의 배수가 아니라면????
+x 그 자체로 x의 배수이자 y의 배수가 아닌 거시다.
 */
 
 #include <cstdio>
